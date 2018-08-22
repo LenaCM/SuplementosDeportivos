d958adc8116836908fd35cfc6827a1c058f5c9eb
VaneDC <kamui713@gmail.com>
2018-08-21 23:37:29 -0300

    Se unifico la view para crear y listar produtos

--- productos/api/views.py
+++ productos/api/views.py
@@ -1,14 +1,17 @@
 from productos.models import Producto
-from rest_framework import generics
+from rest_framework import generics, mixins
 from .serializers import ProductoSerializer
 
-class ProductoCreate(generics.CreateAPIView):
+class ProductoCreate(mixins.CreateModelMixin, generics.ListAPIView):
     
     lookup_field='pk'
     serializer_class = ProductoSerializer
     
     def get_queryset(self):
         return Producto.objects.all()
+    
+    def post(self,request,*args,**kwargs):
+        return self.create(request,*args,**kwargs)
 
 class ProductoRUD(generics.RetrieveUpdateDestroyAPIView):
     
