c53d6fa3190934ac0d0a71924f8a5caaf2320fb5
VaneDC <kamui713@gmail.com>
2018-08-21 23:20:43 -0300

    En productos/api se crearon las views ProductoCreate y ProductoRUD

--- productos/api/views.py
+++ productos/api/views.py
@@ -1,12 +1,22 @@
 from productos.models import Producto
-from rest_framework import viewsets
+from rest_framework import generics
 from .serializers import ProductoSerializer
 
-class ProductoViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
-    queryset = Producto.objects.all().order_by('nombre')
+class ProductoCreate(generics.CreateAPIView):
+    
+    lookup_field='pk'
     serializer_class = ProductoSerializer
+    
+    def get_queryset(self):
+        return Producto.objects.all()
+
+class ProductoRUD(generics.RetrieveUpdateDestroyAPIView):
+    
+    lookup_field='pk'
+    serializer_class = ProductoSerializer
+    
+    def get_queryset(self):
+        return Producto.objects.all().order_by('nombre')
+    
 
     
