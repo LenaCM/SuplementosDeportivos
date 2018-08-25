53578b53780eaf00868341ee5e0f290b4ec0487c
John Doeactivate <kamui713@gmail.com>
2018-08-25 01:22:53 -0300

    se elimino la app mediosDePago

--- mediosDePago/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import MedioDePago
-
-# Register your models here.
-admin.site.register(MedioDePago) 
--- mediosDePago/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MediosdepagoConfig(AppConfig):
-    name = 'mediosDePago'
-    verbose_name = 'Gestinar medios de pago'
