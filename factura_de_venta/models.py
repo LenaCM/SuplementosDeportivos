c43989ed56581413d16fa6ac1d7aeace0cfa4e3d
John Doeactivate <kamui713@gmail.com>
2018-08-25 01:19:04 -0300

    se agrego al modelo de factura_de_venta los modelos medio_de_pago y tipo_de_venta

--- factura_de_venta/models.py
+++ factura_de_venta/models.py
@@ -1,8 +1,5 @@
 from django.db import models
 from django.utils import timezone
-#importo modelos
-from mediosDePago.models import MedioDePago
-from tiposDeVenta.models import TipoDeVenta
 from productos.models import Producto
 
 # Create your models here.
@@ -24,11 +21,30 @@ class TipoDeVenta(models.Model):
 
     def __str__(self):
         return self.tipo_de_venta
-        
+
+class MedioDePago(models.Model):
+    
+    EFECTIVO = 'efectivo'
+    TARJETA = 'tarjeta'
+    
+    OPCIONES_MEDIO_DE_PAGO = (
+        (EFECTIVO, 'efectivo'),
+        (TARJETA, 'tarjeta'),
+    )
+
+    medio_de_pago = models.CharField(max_length=10, choices=OPCIONES_MEDIO_DE_PAGO, default=EFECTIVO)
+    
+    class Meta:
+        verbose_name = "Medio de pago"
+        verbose_name_plural = "Medios de pago"
+
+    def __str__(self):
+        return self.medio_de_pago
+
 class facturaVenta(models.Model):
-    idFacturaVenta = models.BigAutoField(primary_key=True)
-    idMedioDePago = models.ForeignKey(MedioDePago, on_delete=models.CASCADE, verbose_name="Medio de pago")
-    idTipoDeVenta = models.ForeignKey(TipoDeVenta, on_delete=models.CASCADE, verbose_name="tipo de venta")
+    id_factura_de_venta = models.BigAutoField(primary_key=True)
+    id_medio_de_pago = models.ForeignKey(MedioDePago, on_delete=models.CASCADE, verbose_name="Medio de pago")
+    id_tipo_de_venta = models.ForeignKey(TipoDeVenta, on_delete=models.CASCADE, verbose_name="tipo de venta")
     fecha = models.DateTimeField(default=timezone.now)
     total = models.DecimalField(max_digits=10,decimal_places=2)
     
@@ -37,8 +53,8 @@ class facturaVenta(models.Model):
         verbose_name_plural = "Facturas de venta"
 
 class facturaVentaContieneProductos(models.Model):
-    idProducto = models.ForeignKey(Producto, on_delete=models.PROTECT, null=True, blank=True, verbose_name="Producto")
-    idfacturaVenta = models.ForeignKey(facturaVenta, on_delete=models.CASCADE)
+    id_producto = models.ForeignKey(Producto, on_delete=models.PROTECT, null=True, blank=True, verbose_name="Producto")
+    id_factura_de_venta = models.ForeignKey(facturaVenta, on_delete=models.CASCADE)
     precio = models.DecimalField(max_digits=10,decimal_places=2)
     cantidad = models.IntegerField()
     descuento = models.DecimalField(max_digits=10,decimal_places=2)  
